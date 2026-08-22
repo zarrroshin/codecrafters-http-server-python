bda676dfd0d1cc0be757447dcbbb168c13345d25
zahra <zahraroshani973@gmail.com>
2026-08-22 14:07:05 +0330

    feat:Read header

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221))
-
     client_connection, client_address = server_socket.accept()
 
     try:
@@ -14,11 +13,22 @@ def main():
 
         request_lines = request.split("\r\n")
         request_line = request_lines[0]
-        request_line_parts = request_line.split(" ")
 
+        request_line_parts = request_line.split(" ")
         method = request_line_parts[0]
         path = request_line_parts[1]
         version = request_line_parts[2]
+
+        # هدرها را بدون وابستگی به ترتیبشان پیدا می‌کنیم
+        headers_dict = {}
+
+        for line in request_lines[1:]:
+            if not line:  # رسیدن به خط خالی؛ پایان headers
+                break
+
+            if ": " in line:
+                header_name, header_value = line.split(": ", 1)
+                headers_dict[header_name.lower()] = header_value
 
         print(f"Method: {method}")
         print(f"Path: {path}")
@@ -42,6 +52,19 @@ def main():
                 ).encode("utf-8")
 
                 client_connection.sendall(headers + body)
+
+            elif path == "/user-agent":
+                user_agent = headers_dict.get("user-agent", "")
+                body = user_agent.encode("utf-8")
+
+                response_headers = (
+                    "HTTP/1.1 200 OK\r\n"
+                    "Content-Type: text/plain\r\n"
+                    f"Content-Length: {len(body)}\r\n"
+                    "\r\n"
+                ).encode("utf-8")
+
+                client_connection.sendall(response_headers + body)
 
             else:
                 client_connection.sendall(
